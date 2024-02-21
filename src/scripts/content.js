7bd4cc00ee7791e1ff17d7d18bd308a032b68c69
Tengda Yan <teyan@microsoft.com>
2024-02-21 13:06:05 +0800

    Hide banners for feature and task

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,53 +1,66 @@
 
 bugState = document.getElementById("witc_3_txt");
 
-function ShowBaner(){
+function ShowBaner() {
     element = document.querySelector(".project-header");
-    if(element) element.style.removeProperty("display");
-    element = document.querySelector(".work-item-form-main-header");
-    if(element) element.style.removeProperty("display");
+    if (element) element.style.removeProperty("display");
     element = document.querySelector(".vss-PivotBar--bar-two-line");
-    if(element) element.style.removeProperty("display");
-    element = document.querySelector(".work-item-responsive-header-left"); 
-    if(element) element.style.removeProperty("display");
+    if (element) element.style.removeProperty("display");
+
+    elements = document.querySelectorAll(".work-item-form-main-header");
+    if (elements) {
+        elements.forEach((i) => { i.style.removeProperty("display"); });
+    }
+
+    elements = document.querySelectorAll(".work-item-responsive-header-left");
+    if (elements) {
+        elements.forEach((i) => { i.style.removeProperty("display"); });
+    }
 
     brief = document.getElementById("buggest_brief");
-    if(brief) brief.parentNode.removeChild(brief);
+    if (brief) brief.parentNode.removeChild(brief);
 
 }
 
-function HideBanner(){
+function HideBanner() {
     element = document.querySelector(".project-header");
-    if(element) element.style.display = "none";
-    element = document.querySelector(".work-item-form-main-header");
-    if(element) element.style.display = "none";
+    if (element) element.style.display = "none";
     element = document.querySelector(".vss-PivotBar--bar-two-line");
-    if(element) element.style.display = "none";
-    element = document.querySelector(".work-item-responsive-header-left");
-    if(element) element.style.display = "none";
-    element = document.querySelector(".work-item-responsive-header");
-    if(element) element.style.minHeight = "0";
-    
+    if (element) element.style.display = "none";
+    elements = document.querySelectorAll(".work-item-form-main-header");
+    if (elements) {
+        elements.forEach((i) => { i.style.display = "none"; });
+    }
+    elements = document.querySelectorAll(".work-item-responsive-header-left");
+    if (elements) {
+        elements.forEach((i) => { i.style.display = "none"; });
+    }
+
+    elements = document.querySelectorAll(".work-item-responsive-header");
+    if (elements) {
+        elements.forEach((i) => { i.style.minHeight = "0"; });
+    }
+
     bugState = document.getElementById("witc_3_txt");
     bugTitle = document.getElementById("witc_1_txt");
     infotext = document.getElementsByClassName("info-text-wrapper")[0];
 
     bugbrief = document.createElement("div");
     bugbrief.id = "buggest_brief";
-    bugbrief.innerHTML = bugTitle.value + "Statue: " + bugState.value;
+    bugbrief.innerHTML = bugTitle.value + " Statue: " + bugState.value;
     infotext?.appendChild(bugbrief);
-    
+
 }
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(request.action);
-    if(request.action == "show_banner") {
-        ShowBaner();
-    }
-    if(request.action == "hide_banner") {
-        HideBanner();
-    }
-    sendResponse({farewell: "Roger"});
+    function (request, sender, sendResponse) {
+        console.log(request.action);
+        if (request.action == "show_banner") {
+            ShowBaner();
+        }
+        if (request.action == "hide_banner") {
+            HideBanner();
+        }
+        sendResponse({ farewell: "Roger" });
     }
 );
